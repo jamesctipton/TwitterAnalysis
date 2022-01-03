c05542c9e5b40c499c50c68be86767f6805f6c5e
jamtip <jamesc.tipton@gmail.com>
2022-01-02 23:00:57 -0600

    fixed a bug in the JS that couldn't properly load tweet texts
    I also made it so that it loads the whole tweet data as a JSON object into the JS, which makes things easier

--- twitterAnalysis.py
+++ twitterAnalysis.py
@@ -4,42 +4,26 @@ import tornado.web
 import pymongo
 import datetime
 import os.path
-
-# serverURL = '119.45.163.114'
-# user = 'james'
-# password = 'IYT7i6rfTR&%R*&'
-# localport = '127.0.0.1'
+import json
+import bson
+import pprint
 
 client = pymongo.MongoClient("mongodb://james:IYT7i6rfTR&%25R*&@119.45.163.114:27017/TweetScraper")
 
 db = client['TweetScraper']
 collection = db['tweetday']
 
-# print(collection.find_one())
-
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
-        _id = []
-        created_at = []
-        text = []
-        favorites = []
-        retweets = []
-        username = []
-        url = []
-
-        limit = 500
+        
+        tweets = []
+        limit = 20
 
         for tweet in collection.find():
-            _id.append(tweet['_id'])
-            d = datetime.datetime.fromisoformat(tweet['created_at'].replace("Z",""))
-            created_at.append(d.strftime("%B-%d-%Y // %I:%M:%S %p"))
-            text.append(tweet['text'] + "")
-            favorites.append(tweet['like_count'])
-            retweets.append(tweet['retweet_cou'])
-            url.append(tweet['url'])
-            if len(_id) == limit: break
+            tweets.append(json.dumps(tweet))
+            if len(tweets) == limit: break
 
-        self.render("index.html", _id = _id, created_at = created_at, text = text, retweets = retweets, favorites = favorites, url = url, limit = limit)    
+        self.render("index.html", tweets=tornado.escape.json_encode(tweets), limit=limit)    
 
 class Application(tornado.web.Application):
     def __init__(self):
